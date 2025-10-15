9ab41916ecba21144d2780d7c3f81082abf1c3f6
ryo.yoshii <ryo.yoshii@mixi.co.jp>
2025-10-15 23:08:02 +0900

    build プロセスを改善

--- push-ecr.sh
+++ push-ecr.sh
@@ -4,38 +4,31 @@ export REPOSITORY_PREFIX=${ACCOUNT}.dkr.ecr.${REGION}.amazonaws.com
 aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${REPOSITORY_PREFIX}
 
 aws ecr create-repository --repository-name springcommunity/spring-petclinic-api-gateway --region ${REGION} --no-cli-pager || true
-docker pull public.ecr.aws/u2n5q9y7/spring-petclinic-api-gateway:latest
-docker tag public.ecr.aws/u2n5q9y7/spring-petclinic-api-gateway:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-api-gateway:latest
+docker tag springcommunity/spring-petclinic-api-gateway:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-api-gateway:latest
 docker push ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-api-gateway:latest
 
 aws ecr create-repository --repository-name springcommunity/spring-petclinic-discovery-server --region ${REGION} --no-cli-pager || true
-docker pull public.ecr.aws/u2n5q9y7/spring-petclinic-discovery-server:latest
-docker tag public.ecr.aws/u2n5q9y7/spring-petclinic-discovery-server:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-discovery-server:latest
+docker tag springcommunity/spring-petclinic-discovery-server:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-discovery-server:latest
 docker push ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-discovery-server:latest
 
 aws ecr create-repository --repository-name springcommunity/spring-petclinic-config-server --region ${REGION} --no-cli-pager || true
-docker pull public.ecr.aws/u2n5q9y7/spring-petclinic-config-server:latest
-docker tag public.ecr.aws/u2n5q9y7/spring-petclinic-config-server:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-config-server:latest
+docker tag springcommunity/spring-petclinic-config-server:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-config-server:latest
 docker push ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-config-server:latest
 
 aws ecr create-repository --repository-name springcommunity/spring-petclinic-visits-service --region ${REGION} --no-cli-pager || true
-docker pull public.ecr.aws/u2n5q9y7/spring-petclinic-visits-service:latest
-docker tag public.ecr.aws/u2n5q9y7/spring-petclinic-visits-service:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-visits-service:latest
+docker tag springcommunity/spring-petclinic-visits-service:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-visits-service:latest
 docker push ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-visits-service:latest
 
 aws ecr create-repository --repository-name springcommunity/spring-petclinic-vets-service --region ${REGION} --no-cli-pager || true
-docker pull public.ecr.aws/u2n5q9y7/spring-petclinic-vets-service:latest
-docker tag public.ecr.aws/u2n5q9y7/spring-petclinic-vets-service:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-vets-service:latest
+docker tag springcommunity/spring-petclinic-vets-service:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-vets-service:latest
 docker push ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-vets-service:latest
 
 aws ecr create-repository --repository-name springcommunity/spring-petclinic-customers-service --region ${REGION} --no-cli-pager || true
-docker pull public.ecr.aws/u2n5q9y7/spring-petclinic-customers-service:latest
-docker tag public.ecr.aws/u2n5q9y7/spring-petclinic-customers-service:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-customers-service:latest
+docker tag springcommunity/spring-petclinic-customers-service:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-customers-service:latest
 docker push ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-customers-service:latest
 
 aws ecr create-repository --repository-name springcommunity/spring-petclinic-admin-server --region ${REGION} --no-cli-pager || true
-docker pull public.ecr.aws/u2n5q9y7/spring-petclinic-admin-server:latest
-docker tag public.ecr.aws/u2n5q9y7/spring-petclinic-admin-server:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-admin-server:latest
+docker tag springcommunity/spring-petclinic-admin-server:latest ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-admin-server:latest
 docker push ${REPOSITORY_PREFIX}/springcommunity/spring-petclinic-admin-server:latest
 
 
